d18cff99b83439495093a3cddfebddaf7a52afeb
Do In Hyeok <ehdlsgur99@naver.com>
2023-01-10 17:48:34 +0900

    create test actor

---
+++ Source/Journey/testyActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "testyActor.h"
+
+// Sets default values
+AtestyActor::AtestyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AtestyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AtestyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
