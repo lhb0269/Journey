beaaed12988ca834bd7dd665d65231409a3aa839
Hanbit Lee <lhb0269@gmail.com>
2023-01-10 17:40:37 +0900

    Initialize Project

---
+++ Source/Journey/JourneyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JourneyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JOURNEY_API AJourneyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
